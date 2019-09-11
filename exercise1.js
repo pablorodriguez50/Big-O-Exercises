d26b8ed27423c64e99821972792d0df3632f4831
PABLO RODRIGUEZ <pablorodrigueza@hotmail.cl>
2019-09-11 14:42:16 -0300

    Initital Commit

---
+++ exercise1.js
@@ -0,0 +1,22 @@
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+
+const large = new Array(1000).fill('nemo');
+
+function findNemo(array){
+    let t0 = performance.now();
+    for (let i = 0; i < array.length; i++){
+        if(array[i] == 'nemo'){
+            console.log('Found NEMO!');
+        }
+    }
+
+    let t1 = performance.now();
+    console.log('Call to find Nemo took ' + (t1-t0) + ' miliseconds');
+}
+
+findNemo(large); // 0(n) --> Linear Time
+
+
+// O(n) --> Linear Time
+// O(n^2) --> Quadratic Time
